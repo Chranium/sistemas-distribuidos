a90623cdf481043cef1655ffd5b3ff111050db24
Miguel Medellín <mamedellinr@unal.edu.co>
2022-02-01 20:31:07 -0500

    update code for linux

--- OpenMPI/ImageFilter/imageFilter.cpp
+++ OpenMPI/ImageFilter/imageFilter.cpp
@@ -17,8 +17,8 @@ int main() {
     int x, y, count;
 
     // Open an input pipe from ffmpeg and an output pipe to a second instance of ffmpeg
-    FILE* pipein = _popen("ffmpeg -i teapot_input.mp4 -f image2pipe -vcodec rawvideo -pix_fmt rgb24 -", "rb+");
-    FILE* pipeout = _popen("ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt rgb24 -s 1280x720 -r 25 -i - -f mp4 -q:v 5 -an -vcodec mpeg4 teapot_output.mp4", "wb+");
+    FILE* pipein = popen("ffmpeg -i teapot_input.mp4 -f image2pipe -vcodec rawvideo -pix_fmt rgb24 -", "rb+");
+    FILE* pipeout = popen("ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt rgb24 -s 1280x720 -r 25 -i - -f mp4 -q:v 5 -an -vcodec mpeg4 teapot_output.mp4", "wb+");
 
     clock_t begin = clock();
 
@@ -51,8 +51,8 @@ int main() {
 
     // Flush and close input and output pipes
     fflush(pipein);
-    _pclose(pipein);
+    pclose(pipein);
     fflush(pipeout);
-    _pclose(pipeout);
+    pclose(pipeout);
     return 0;
 }
